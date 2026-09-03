c8e5338f00eb588aaa31646cdce7c0c4eedb383a
noureddine makboul <noureddinemakboul03@gmail.com>
2026-09-03 04:08:05 +0100

    Add RAIIDE Media and M2 Maroc logos to partner marquee

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,9 @@ const partnersList = [
   { name: "REDMED", img: `${import.meta.env.BASE_URL}partner_redmed.png` },
   { name: "Universal PF", img: `${import.meta.env.BASE_URL}partner_universal_pf.png` },
   { name: "Vogal Store", img: `${import.meta.env.BASE_URL}partner_vogal.png` },
-  { name: "TRIPWAY", img: `${import.meta.env.BASE_URL}partner_tripway.png` }
+  { name: "TRIPWAY", img: `${import.meta.env.BASE_URL}partner_tripway.png` },
+  { name: "RAIIDE Media", img: `${import.meta.env.BASE_URL}partner_raiide_media.png` },
+  { name: "M2 Maroc", img: `${import.meta.env.BASE_URL}partner_m2_maroc.png` }
 ];
 
 const servicesList = [
